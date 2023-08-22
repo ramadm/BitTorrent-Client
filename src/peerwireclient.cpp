a0946b9229a1769b74fefb13ef1ee2b2aa27b2bb
Rama Del Maestro <rdelmaestro@gmail.com>
2023-08-22 13:36:55 -0700

    Cleaned up handshake processing

--- src/peerwireclient.cpp
+++ src/peerwireclient.cpp
@@ -14,10 +14,11 @@ PeerWireClient::PeerWireClient(asio::io_context &ioc, std::string addrStr, std::
     port = (asio::ip::port_type)portStr[0] << 8 | 
         (asio::ip::port_type)portStr[1];
     peerPrefix = "[Peer " + std::to_string(peerNumber) + "] ";
+    peerValidated = false;
     startConnection();
 }
 
-// TODO: should we connect immediately and how to handle refused connections?
+// TODO: how should we handle refused connections?
 void PeerWireClient::startConnection() {
     asio::ip::tcp::endpoint endpoint(addr, port);
     auto callback = std::bind(&PeerWireClient::handleConnect, this, std::placeholders::_1);
@@ -55,40 +56,67 @@ void PeerWireClient::handleRead(const asio::error_code &error, size_t bytesRead)
         std::cerr << peerPrefix << "Read err: " << error.message() << std::endl;
     } else {
         std::cout << peerPrefix << "Received response (" << bytesRead << " bytes)\n";
-
-        unsigned char pStrLen = messageBuffer[0];
-        std::string pStr = "";
-        for (size_t i = 0; i < pStrLen; i++) {
-            pStr += messageBuffer[i+1];
+        if (!peerValidated) {
+            checkHandshake(bytesRead);
+            if (!socket.is_open()) {
+                return;
+            }
+            std::cout << "After trimming handshake, received message:\n";
+            std::cout.write(messageBuffer.data(), bytesRead);
+            std::cout << " (" << bytesRead << " bytes)\n";
         }
-        // TODO: reserved bits
-        peerID = "";
-        std::string peerInfoHash = "";
-        for (size_t i = 0; i < 20; i++) {
-            peerInfoHash += messageBuffer[i+pStrLen+9];
-            peerID += messageBuffer[i+pStrLen+29];
-        }
-        
-        if (infoHash != peerInfoHash) {
-            std::cout << peerPrefix << "Incorrect info hash!\n";
-            // TODO: drop connection
-            abort();
-        }
-        if (pStr != "BitTorrent protocol") {
-            std::cout << peerPrefix << "Unrecognized protocol version: " << pStr << std::endl;
-            // TODO: drop connection?
-            abort();
-        }
-        
-        std::string remainder = "";
-        for (size_t i = pStrLen + 49; i < bytesRead; i++) {
-            remainder += messageBuffer[i];
-        }
-        std::cout << peerPrefix << remainder << std::endl;
+        // TODO: process the message
     }
 }
+
+// Check that the handshake is valid and adjust messageBuffer and bytesRead to subtract the
+// handshake
+void PeerWireClient::checkHandshake(size_t &bytesRead) {
+    unsigned char pStrLen = messageBuffer[0];
+    std::string pStr = "";
+    for (size_t i = 0; i < pStrLen; i++) {
+        pStr += messageBuffer[i+1];
+    }
+    // TODO: handle reserved bits
+    peerID = "";
+    std::string peerInfoHash = "";
+    for (size_t i = 0; i < 20; i++) {
+        peerInfoHash += messageBuffer[i+pStrLen+9];
+        peerID += messageBuffer[i+pStrLen+29];
+    }
+    
+    if (infoHash != peerInfoHash) {
+        std::cerr << peerPrefix << "Incorrect info hash!\n";
+        // drop the connection
+        asio::error_code ec;
+        socket.shutdown(asio::ip::tcp::socket::shutdown_both, ec);
+        if (ec) { std::cerr << ec.message() << std::endl; }
+        socket.close();
+        return;
+    }
+    if (pStr != "BitTorrent protocol") {
+        std::cerr << peerPrefix << "Unrecognized protocol version: " << pStr << std::endl;
+        // drop the connection
+        asio::error_code ec;
+        socket.shutdown(asio::ip::tcp::socket::shutdown_both, ec);
+        if (ec) { std::cerr << ec.message() << std::endl; }
+        socket.close();
+        return;
+    }
+
+    bytesRead = bytesRead - (pStrLen + 49);
+    std::array<char, MSG_BUF_SIZE> temp;
+    for (size_t i = 0; i < bytesRead; i++) {
+        temp[i] = messageBuffer[i + pStrLen + 49];
+    }
+    std::copy_n(std::begin(temp), bytesRead, std::begin(messageBuffer));
+    peerValidated = true;
+}
+
 
 // We receive handshake in response, followed by some more information I need to parse. Looks to be
 // a ton of binary
 // TODO: send choking/interested info over
 // TODO: start downloading pieces!!
+
+// Note: The specification recommends requesting 16KB at a time.
--- src/peerwireclient.h
+++ src/peerwireclient.h
@@ -1,5 +1,19 @@
 #include <iostream>
 #include "external/asio-1.28.0/include/asio.hpp"
+#define MSG_BUF_SIZE 1024
+
+enum MessageID : unsigned char {
+    Choke = 0,
+    Unchoke = 1,
+    Interested = 2,
+    NotInterested = 3,
+    Have = 4,
+    Bitfield = 5,
+    Request = 6,
+    Piece = 7,
+    Cancel = 8,
+    Port = 9 // may not be used frequently
+};
 
 class PeerWireClient {
 public:
@@ -7,8 +21,6 @@ public:
         std::string ih);
 
 private:
-    // TODO: copying this from boost's example, but I believe ioContext is a reference variable
-    // because a single io_context object is maintained between all connections
     size_t peerNumber;
     std::string peerPrefix;
     std::string peerID;
@@ -18,9 +30,12 @@ private:
     asio::ip::port_type port;
     asio::io_context& ioContext;
     asio::ip::tcp::socket socket;
-    std::array<char, 128> messageBuffer;
+    std::array<char, MSG_BUF_SIZE> messageBuffer;
+    bool peerValidated;
     void startConnection();
     void handleConnect(const asio::error_code &error);
     void handleWrite(const asio::error_code &error, size_t bytesWritten);
+    void handleFirstRead(const asio::error_code &error, size_t bytesRead);
     void handleRead(const asio::error_code &error, size_t bytesRead);
+    void checkHandshake(size_t &bytesRead);
 };
